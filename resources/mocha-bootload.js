f5fb6de597d222c81ba1ed32b42ce0d18258cff9
nodkz <nodkz@mail.ru>
2016-06-30 20:04:45 +0600

    Add chai-spies for testing helpers

--- resources/mocha-bootload.js
+++ resources/mocha-bootload.js
@@ -1,7 +1,11 @@
+/* eslint-disable */
+
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
+import chaiSpies from 'chai-spies';
 
 chai.use(chaiAsPromised);
+chai.use(chaiSpies);
 
 process.on('unhandledRejection', (error) => {
   console.error('Unhandled Promise Rejection:');
