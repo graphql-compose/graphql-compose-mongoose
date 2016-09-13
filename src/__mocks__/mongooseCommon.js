ec61bd5374ecf0f274c19c4b217e05b6aa416cf7
nodkz <nodkz@mail.ru>
2016-09-13 17:18:20 +0600

    chore(build): Random database names for Travis multi-tests
    Tests may break on same database

--- src/__mocks__/mongooseCommon.js
+++ src/__mocks__/mongooseCommon.js
@@ -1,6 +1,10 @@
 import mongoose, { Schema } from 'mongoose';
 
-const dbName = 'gqc-mongoose-test';
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const dbName = `gqc-mongoose-test${getRandomInt(1, 1000000)}`;
 const uri = `mongodb://127.0.0.1:27017/${dbName}`;
 
 mongoose.Promise = Promise;
