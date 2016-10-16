0bc78ddd7ad20601b1f47ba6e970c3356a6f0c68
nodkz <nodkz@mail.ru>
2016-10-16 21:06:21 +0600

    fix(ProjectionHelper): Avoid flatting for MongoDB Projection Operators
    https://docs.mongodb.com/v3.2/reference/operator/projection/meta/

--- src/resolvers/helpers/__tests__/projection-test.js
+++ src/resolvers/helpers/__tests__/projection-test.js
@@ -21,10 +21,38 @@ describe('Resolver helper `projection` ->', () => {
       projectionHelper(resolveParams);
       expect(spyFn).to.have.not.been.called();
     });
+
     it('should call query.select if projection is provided', () => {
       resolveParams.projection = { name: 1, age: 1 };
       projectionHelper(resolveParams);
       expect(spyFn).to.have.been.called.with({ name: true, age: true });
+    });
+
+    it('should make projection fields flat', () => {
+      resolveParams.projection = { name: { first: 1, last: 1 } };
+      projectionHelper(resolveParams);
+      expect(spyFn).to.have.been.called.with({ name: true });
+    });
+
+    describe('projection operators', () => {
+      // see more details here https://docs.mongodb.com/v3.2/reference/operator/projection/meta/
+      it('should pass $meta unflatted', () => {
+        resolveParams.projection = { score: { $meta: 'textScore' } };
+        projectionHelper(resolveParams);
+        expect(spyFn).to.have.been.called.with({ score: { $meta: 'textScore' } });
+      });
+
+      it('should pass $slice unflatted', () => {
+        resolveParams.projection = { comments: { $slice: 5 } };
+        projectionHelper(resolveParams);
+        expect(spyFn).to.have.been.called.with({ comments: { $slice: 5 } });
+      });
+
+      it('should pass $elemMatch unflatted', () => {
+        resolveParams.projection = { students: { $elemMatch: { school: 102 } } };
+        projectionHelper(resolveParams);
+        expect(spyFn).to.have.been.called.with({ students: { $elemMatch: { school: 102 } } });
+      });
     });
   });
 });
--- src/resolvers/helpers/projection.js
+++ src/resolvers/helpers/projection.js
@@ -8,8 +8,14 @@ export function projectionHelper(resolveParams: ExtendedResolveParams): void {
   const projection = resolveParams.projection;
   if (projection) {
     const flatProjection = {};
-    Object.keys(projection).forEach(key => {
-      flatProjection[key] = !!projection[key];
+    Object.keys(projection).forEach((key) => {
+      if (projection[key].$meta || projection[key].$slice || projection[key].$elemMatch) {
+        // pass MongoDB projection operators https://docs.mongodb.com/v3.2/reference/operator/projection/meta/
+        flatProjection[key] = projection[key];
+      } else {
+        // if not projection operator, then flatten projection
+        flatProjection[key] = !!projection[key];
+      }
     });
     resolveParams.query = resolveParams.query.select(flatProjection); // eslint-disable-line
   }
