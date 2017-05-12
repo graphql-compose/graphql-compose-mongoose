7a6ac2de747d14281f9965f418065e97a57cfb37
nodkz <pavel.chertorogov@gmail.com>
2017-05-12 11:10:47 +0600

    chore: Use mongodb-memory-server package for spinning up a test MongoDB Server

--- src/__mocks__/mongooseCommon.js
+++ src/__mocks__/mongooseCommon.js
@@ -1,41 +1,29 @@
 /* eslint-disable no-param-reassign, no-console */
 import mongoose, { Schema } from 'mongoose';
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-const dbName = `gqc-mongoose-test${getRandomInt(1, 1000000)}`;
-const uri = `mongodb://127.0.0.1:27017/${dbName}`;
+import MongodbMemoryServer from 'mongodb-memory-server';
 
 mongoose.Promise = Promise;
-mongoose.connect(uri);
 
-mongoose.connection.on('error', (err) => {
-  throw new Error(err);
+const mongoServer = new MongodbMemoryServer();
+
+mongoServer.getConnectionString().then((mongoUri) => {
+  mongoose.connect(mongoUri);
+
+  mongoose.connection.on('error', (e) => {
+    if (e.message.code === 'ETIMEDOUT') {
+      console.log(e);
+      mongoose.connect(mongoUri);
+    } else {
+      throw e;
+    }
+  });
+
+  mongoose.connection.once('open', () => {
+    console.log(`MongoDB successfully connected to ${mongoUri}`);
+  });
 });
-
-mongoose.connection.on('connected', () => {
-  console.log(`Mongoose default connection open to ${uri}`);
-});
-
-function dropDBs(done) {
-  try {
-    mongoose.connection.db.dropDatabase(() => {
-      if (done) done();
-    });
-  } catch (e) { // Pitty, but not deathly
-    if (done) done();
-  }
-}
-
-process.on('exit', () => { dropDBs(); });
-process.on('SIGINT', () => { dropDBs(); });
 
 export {
   mongoose,
   Schema,
-  dropDBs,
-  uri,
-  dbName,
 };
