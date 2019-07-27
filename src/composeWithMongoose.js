7f8455f71a38567966024825db46121bdcca6f46
Morgan Touverey Quilling (Basalte) <mtouverey@methodinthemadness.eu>
2019-07-28 00:13:05 +0200

    chore: fix flow/ts typings of TypeConverterResolversOpts.updateOne

--- src/composeWithMongoose.js
+++ src/composeWithMongoose.js
@@ -70,7 +70,7 @@ export type TypeConverterResolversOpts = {
   updateOne?:
     | false
     | {
-        input?: RecordHelperArgsOpts | false,
+        record?: RecordHelperArgsOpts | false,
         filter?: FilterHelperArgsOpts | false,
         sort?: SortHelperArgsOpts | false,
         skip?: false,
