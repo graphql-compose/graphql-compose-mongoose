dfa0e2182e694ddd3a87a32493adc82ac13d2e8f
antoniopresto <antoniopresto@gmail.com>
2020-01-21 04:54:24 -0300

    fix: disable default required type for mongoose conditional required fields (#204)

--- src/resolvers/createOne.js
+++ src/resolvers/createOne.js
@@ -26,7 +26,7 @@ export default function createOne<TSource: MongooseDocument, TContext>(
   for (const field in tree) {
     if (tree.hasOwnProperty(field)) {
       const fieldOptions = tree[field];
-      if (fieldOptions.required) {
+      if (fieldOptions.required && typeof fieldOptions.required !== 'function') {
         requiredFields.push(field);
       }
     }
