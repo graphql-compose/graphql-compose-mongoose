e69782961cc9c1a29706ac209f3dbab3e3ec9928
nodkz <pavel.chertorogov@gmail.com>
2020-09-03 23:48:32 +0600

    refactor: ValidationError should not be a GraphQLError object. It may be rewrapped in `addErrorCatcherField()` if client does not request `error` field in payload. We are using GraphQLError only for providing `extensions` property on top-level.

--- src/errors/ValidationError.ts
+++ src/errors/ValidationError.ts
@@ -1,23 +1,18 @@
 import { SchemaComposer, ObjectTypeComposer } from 'graphql-compose';
-import { GraphQLError } from 'graphql';
 import type { ValidationErrorData, ValidationsWithMessage } from '../resolvers/helpers/validate';
 
-export class ValidationError extends GraphQLError {
+export class ValidationError extends Error {
   public errors: ValidationErrorData[];
 
   constructor(validation: ValidationsWithMessage) {
-    super(validation.message, undefined, undefined, undefined, undefined, undefined, {
-      validations: validation.errors,
-    });
-
+    super(validation.message);
     this.errors = validation.errors;
-
     (this as any).__proto__ = ValidationError.prototype;
   }
 }
 
-export function getValidationOTC(schemaComposer: SchemaComposer<any>): ObjectTypeComposer {
-  return schemaComposer.getOrCreateOTC('Validation', (otc) => {
+export function getValidatorErrorOTC(schemaComposer: SchemaComposer<any>): ObjectTypeComposer {
+  return schemaComposer.getOrCreateOTC('ValidatorError', (otc) => {
     otc.addFields({
       message: {
         description: 'Validation error message',
@@ -31,13 +26,18 @@ export function getValidationOTC(schemaComposer: SchemaComposer<any>): ObjectTyp
         description: 'Field value which occurs the validation error',
         type: 'JSON',
       },
+      idx: {
+        description:
+          'Input record idx in array which occurs the validation error. This `idx` is useful for createMany operation. For singular operations it always be 0. For *Many operations `idx` represents record index in array received from user.',
+        type: 'Int!',
+        resolve: (s: any) => s.idx || 0,
+      },
     });
   });
 }
 
 export function getValidationErrorOTC(schemaComposer: SchemaComposer<any>): ObjectTypeComposer {
   return schemaComposer.getOrCreateOTC('ValidationError', (otc) => {
-    const Validation = getValidationOTC(schemaComposer);
     otc.addFields({
       message: {
         description: 'Combined error message from all validators',
@@ -45,7 +45,7 @@ export function getValidationErrorOTC(schemaComposer: SchemaComposer<any>): Obje
       },
       errors: {
         description: 'List of validator errors',
-        type: Validation.NonNull.List,
+        type: getValidatorErrorOTC(schemaComposer).NonNull.List,
       },
     });
   });
