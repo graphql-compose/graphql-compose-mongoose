eae50ea106cabb25d09925fa927e0ccff654877e
nodkz <pavel.chertorogov@gmail.com>
2020-09-08 01:16:50 +0600

    test: add integration test for `dataLoaderXXX` resolvers

--- src/resolvers/helpers/dataLoaderHelper.ts
+++ src/resolvers/helpers/dataLoaderHelper.ts
@@ -15,19 +15,34 @@ export function getDataLoader(
   // get or create DataLoader in GraphQL context
   let dl: DataLoader<any, any> = _gqlDataLoaders.get(dlKey);
   if (!dl) {
+    const dataLoaderOptions = {
+      cacheKeyFn: (k) => {
+        if (k?.equals) {
+          // Convert ObjectId to string for combining different instances of same ObjectIds.
+          // Eg. you have 10 articles with same authorId. So in memory `authorId` for every record
+          //     will have its own instance of ObjectID.
+          //
+          // mongoose will convert them back to ObjectId automatically when call `find` method
+          return k.toString();
+        }
+        return k;
+      },
+    } as DataLoader.Options<any, any, any>;
+
     dl = new DataLoader(async (ids) => {
       const result = await batchLoadFn(ids);
       // return docs in the same order as were provided their ids
       return ids.map((id) =>
-        (result as any).find((d: any) => {
-          if (d?._id?.equals) {
+        (result as any).find((doc: any) => {
+          if (doc?._id?.equals) {
             // compare correctly MongoIDs via ObjectID.equals() method
-            return d._id.equals(id);
+            return doc._id.equals(id);
           }
-          return d._id === id;
+          return doc._id === id;
         })
       );
-    });
+    }, dataLoaderOptions);
+
     _gqlDataLoaders.set(dlKey, dl);
   }
   return dl;
