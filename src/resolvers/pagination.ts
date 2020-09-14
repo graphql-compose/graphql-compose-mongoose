611d65ff41a44218ad2966fec3b0395ec881d853
nodkz <pavel.chertorogov@gmail.com>
2020-09-14 19:49:11 +0600

    fix: refactor `pagination` & `connection` resolvers. Now they are using `count` & `findMany` resolver instances instead of getting them by `string`-name. Move them from `peerDependencies` to `dependencies`.

--- src/resolvers/pagination.ts
+++ src/resolvers/pagination.ts
@@ -1,41 +1,31 @@
 import type { Resolver, ObjectTypeComposer } from 'graphql-compose';
 import type { Model, Document } from 'mongoose';
+import {
+  preparePaginationResolver,
+  PaginationTArgs,
+  PaginationResolverOpts as _PaginationResolverOpts,
+} from 'graphql-compose-pagination';
+import { CountResolverOpts, count } from './count';
+import { FindManyResolverOpts, findMany } from './findMany';
 
-export type PaginationResolverOpts = {
-  perPage?: number;
-};
-
-type TArgs = {
-  page?: number;
-  perPage?: number;
-  filter?: any;
-  sort?: Record<string, any>;
+export type PaginationResolverOpts = Omit<
+  _PaginationResolverOpts,
+  'countResolver' | 'findManyResolver'
+> & {
+  countOpts?: CountResolverOpts;
+  findManyOpts?: FindManyResolverOpts;
 };
 
 export function pagination<TSource = any, TContext = any, TDoc extends Document = any>(
-  _model: Model<TDoc>,
+  model: Model<TDoc>,
   tc: ObjectTypeComposer<TDoc, TContext>,
   opts?: PaginationResolverOpts
-): Resolver<TSource, TContext, TArgs, TDoc> | undefined {
-  try {
-    require.resolve('graphql-compose-pagination');
-  } catch (e) {
-    return undefined;
-  }
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const preparePaginationResolver = require('graphql-compose-pagination').preparePaginationResolver;
-
-  if (!preparePaginationResolver) {
-    throw new Error(
-      'You should update `graphql-compose-pagination` package till 3.3.0 version or above'
-    );
-  }
-
-  const resolver = preparePaginationResolver(tc, {
-    findResolverName: 'findMany',
-    countResolverName: 'count',
-    ...opts,
+): Resolver<TSource, TContext, PaginationTArgs, TDoc> {
+  const { countOpts, findManyOpts, ...restOpts } = opts || {};
+  const resolver = preparePaginationResolver<any, any>(tc, {
+    findManyResolver: findMany(model, tc, findManyOpts),
+    countResolver: count(model, tc, countOpts),
+    ...restOpts,
   });
-
   return resolver;
 }
